c3abbbe801386d8ddc0079f8a70552a7ad32e020
nurrative <nursul1997@gmail.com>
2023-12-02 20:53:56 +0600

    added redis

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
 from src.auth.base_config import auth_backend, fastapi_users
+from fastapi_cache.backends.redis import RedisBackend
+
+from redis import asyncio as aioredis
 from src.auth.schemas import UserRead, UserCreate
 from src.operations.router import router as router_operation
 
@@ -20,5 +24,11 @@ app.include_router(
 )
 
 app.include_router(router_operation)
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
 # uvicorn main:app  - запуск приложения
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,6 +1,10 @@
+import time
+
 from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert
+from fastapi_cache.decorator import cache
 from src.operations.models import operation
 from src.database import get_async_session
 from .schemas import OperationCreate
@@ -11,11 +15,31 @@ router = APIRouter(
 )
 
 
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Много много данных, которые вычислялись сто лет"
+
+
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    return result.mappings().all()
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        return {
+                'status': 'success',
+                'data': result.mappings().all(),
+                'details': None
+            }
+
+    except Exception:
+        # Передать ошибку разработчикам
+        raise HTTPException(status_code=500, detail={
+            'status': 'error',
+            'data': None,
+            'details': None
+        })
 
 
 @router.post('/')
