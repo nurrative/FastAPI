0afff22ec3ce720997dd180a76cc1c37a4af4b99
nurrative <nursul1997@gmail.com>
2023-12-07 22:21:46 +0600

    added jinja

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
+from fastapi.staticfiles import StaticFiles
 from src.auth.base_config import auth_backend, fastapi_users
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi.middleware.cors import CORSMiddleware
@@ -7,10 +8,13 @@ from redis import asyncio as aioredis
 from src.auth.schemas import UserRead, UserCreate
 from src.operations.router import router as router_operation
 from src.tasks.router import router as router_tasks
+from src.pages.router import router as router_pages
 
 app = FastAPI(
     title="Trading App"
 )
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -35,6 +39,7 @@ async def startup_event():
 
 app.include_router(router_operation)
 app.include_router(router_tasks)
+app.include_router(router_pages)
 
 origins = [
     "http://localhost",
